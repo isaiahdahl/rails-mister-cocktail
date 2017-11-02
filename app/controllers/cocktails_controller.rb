8b90e5ced879ceb27f6860d87ed8ebff63aeb1e5
isaiah dahl <isaiahdahl@me.com>
2017-11-02 14:46:07 +0000

    yay all green

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,49 @@
 class CocktailsController < ApplicationController
+  before_action :find_cocktail, only: [:show, :edit, :update, :destroy]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      # GO BACK TO THE FORM
+      render :new
+    end
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    redirect_to :back
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,31 @@
 class DosesController < ApplicationController
+  def new
+    # we need @restaurant in our `simple_form_for`
+    @cocktail = Cocktail.find(params[:cocktail_id])
+
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    # we need `ingredient_id` to asssociate dose with corresponding restaurant
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(params[:cocktail_id])
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
 end
