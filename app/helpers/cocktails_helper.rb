66742cd8b2f9a178c8b638322b7150286fbe9ab4
isaiah dahl <isaiahdahl@me.com>
2017-11-03 18:13:52 +0000

    new rails app

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -2,11 +2,15 @@ module CocktailsHelper
   def index_photo?(elem)
     if elem.photo?
       (cl_image_path elem.photo.path, height: 300, width: 300, crop: :fit, gravity: :auto)
+    else
+      ('http://lorempixel.com/400/200').html_safe
     end
   end
   def show_photo?(elem)
     if elem.photo?
       (cl_image_path elem.photo.path,height: 300, width: 300, crop: :fit, gravity: :auto)
+    else
+      ('http://lorempixel.com/400/200').html_safe
     end
   end
 end
